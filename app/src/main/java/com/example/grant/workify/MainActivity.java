6c4f345404c9614e10ed2d36971b987286ec7ddb
grantw99 <35930267+grantw99@users.noreply.github.com>
2019-08-22 15:32:33 -0400

    Anyang

--- app/src/main/java/com/example/grant/workify/MainActivity.java
+++ app/src/main/java/com/example/grant/workify/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 //Hi Grant u kinda cute wink
+//no u
 public class MainActivity extends AppCompatActivity {
 
     @Override
